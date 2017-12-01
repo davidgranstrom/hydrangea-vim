0ca032c977a4b8b8e5f0acbc412dbbac2ac3c2ac
David Granström <david@davidgranstrom.com>
2017-12-01 11:24:00 +0100

    Remove bg hightlightning

--- src/hydrangea.py
+++ src/hydrangea.py
@@ -70,9 +70,9 @@ color['NonText']      = {'fg': base00,           'bg': base03,                'd
 color['MatchParen']   = {'fg': red1,             'bg': 'NONE',                'deco': 'bold'     }
 
 color['Comment']      = {'fg': base00,                                        'deco': 'NONE'     }
-color['Constant']     = {'fg': teal2,            "bg": teal01,                'deco': 'NONE'     }
+color['Constant']     = {'fg': teal2,            "bg": 'NONE',                'deco': 'NONE'     }
 color['String']       = 'Constant'
-color['Number']       = {'fg': cyan2,            'bg': cyan01,                'deco': 'NONE'     }
+color['Number']       = {'fg': cyan2,            'bg': 'NONE',                'deco': 'NONE'     }
 color['Identifier']   = {'fg': base3,                                         'deco': 'bold'     }
 color['Function']     = {'fg': base3,                                         'deco': 'bold'     }
 color['Statement']    = {'fg': blue1,                                         'deco': 'bold'     }
